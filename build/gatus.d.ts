a75d1cbaba3ded6ad4cb510bca95d02e14185088
Jannik Bülow <jannikbulow0@gmail.com>
2026-03-18 14:39:07 +0100

    Remove build files

--- build/gatus.d.ts
+++
@@ -1,8 +0,0 @@
-import { LogFunc } from "./logging";
-export interface ExternalEndpointStatus {
-    success: boolean;
-    error?: string;
-    duration?: number;
-}
-export declare function generateGatusURL(hostname: string, endpointKey: string): string;
-export declare function sendExternalEndpointStatus(url: string, token: string, req: ExternalEndpointStatus, log?: LogFunc): Promise<void>;
--- build/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { LogFunc } from "./logging";
-export interface Condition {
-    name: string;
-    run: () => Promise<boolean>;
-}
-export declare function makeCondition(name: string, run: () => Promise<boolean>): Condition;
-export declare function worker(hostname: string, endpointKey: string, token: string, conditions: Condition[], interval?: number, log?: LogFunc): Promise<never>;
-export declare function start(hostname: string, endpointKey: string, token: string, conditions: Condition[], interval?: number, log?: LogFunc): void;
